db2dfa63f8a42d4d1ab1bcf440023a28374afc52
T.Z. <632437335@qq.com>
2024-09-11 13:34:05 +0800

    add skin layers
    shift renderer to custom mob types, adding type and basic layer of villager model, for 2 NPC (professor and librarian)

--- src/main/java/entity/ProfessorNPCEntityRenderer.java
+++ src/main/java/entity/ProfessorNPCEntityRenderer.java
@@ -1,5 +1,7 @@
 package entity;
 
+import entity.layers.ProfPlainLayer;
+import entity.layers.ProfessorLayer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
 import net.minecraft.client.model.geom.ModelLayers;
@@ -12,16 +14,17 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.npc.Villager;
 
 
-public class ProfessorNPCEntityRenderer extends VillagerRenderer {
-    private static final ResourceLocation PROFESSOR_TEXTURE = new ResourceLocation("npcopenai", "textures/entity/villager/profession/engineer.png");
+public class ProfessorNPCEntityRenderer extends MobRenderer<ProfessorNPCEntity, VillagerModel<ProfessorNPCEntity>> {
+    private static final ResourceLocation PROFESSOR_TEXTURE = new ResourceLocation("npcopenai", "textures/entity/villager/villager.png");
 
     public ProfessorNPCEntityRenderer(EntityRendererProvider.Context context) {
-        super(context);
+        super(context, new VillagerModel<>(context.bakeLayer(ModelLayers.VILLAGER)), 0.5f);
+        this.addLayer(new ProfPlainLayer(this));
+        this.addLayer(new ProfessorLayer(this));
     }
 
-
     @Override
-    public ResourceLocation getTextureLocation(Villager entity) {
+    public ResourceLocation getTextureLocation(ProfessorNPCEntity entity) {
         // 你可以根据实体的状态或属性返回不同的纹理
         return PROFESSOR_TEXTURE;
     }
---
+++ src/main/java/entity/layers/ProfessorLayer.java
@@ -0,0 +1,24 @@
+package entity.layers;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import entity.ProfessorNPCEntity;
+import net.minecraft.client.model.VillagerModel;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.resources.ResourceLocation;
+
+public class ProfessorLayer extends RenderLayer<ProfessorNPCEntity, VillagerModel<ProfessorNPCEntity>> {
+    public final static ResourceLocation PROF_TEXTURE = new ResourceLocation("textures/entity/profession/profession.png");
+
+    public ProfessorLayer(RenderLayerParent<ProfessorNPCEntity, VillagerModel<ProfessorNPCEntity>> parent) {
+        super(parent);
+    }
+
+    @Override
+    public void render(PoseStack matrixStack, MultiBufferSource buffer, int packedLight, ProfessorNPCEntity entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+        if (!entity.isInvisible()) {
+            renderColoredCutoutModel(this.getParentModel(), PROF_TEXTURE, matrixStack, buffer, packedLight, entity, 1.0F, 1.0F, 1.0F);
+        }
+    }
+}
